d945defc2f3a65f2f8bf1ccd8a7617abd15a9a84
Trần Thanh Vũ <ttvu90@gmail.com>
2021-04-14 22:36:58 +0700

    Day000: using swiftUI in playground

---
+++ 100DaysOfSwiftUI/Day000.playground/Contents.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import PlaygroundSupport
+
+/// using swiftUI in playground
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
